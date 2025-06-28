73e60b4a712e433eebb87efcd0ddbe0659cbb99b
AKASH <akpersonalmca@gmail.com>
2025-06-28 12:58:55 +0530

    add some variables

--- src/git.java
+++ src/git.java
@@ -1,5 +1,9 @@
 public class git {
     private String newData = "This is my new data";
     private String  hello ;
+    private String checkIt;
+    private String kcheckIt;
+    private String kjcheckIt;
+    private String kjscheckIt;
 }
 
